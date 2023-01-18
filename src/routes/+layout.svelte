061f7884106721766e942a9c328f387a917b1faa
Ryan Scheuer <ryan.scheuer1@gmail.com>
2023-01-17 21:15:43 -0500

    fonts work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,27 @@
 
 <slot />
 
-<style>
+<style lang="postcss">
+	:global(:root) {
+		--font-body: 'IBM Plex Serif', serif;
+		--color-bg-1: #ffffff;
+		--color-theme-1: #9092a9;
+		--color-theme-2: #4075a6;
+		--color-text: rgba(0, 0, 0, 1);
+		--column-width: 42rem;
+		--column-margin-top: 4rem;
+	}
+
+	:global(body) {
+		min-height: 100vh;
+		margin: 0;
+		background-attachment: fixed;
+		background-color: var(--color-bg-1);
+		background-size: 100vw 100vh;
+		font-family: var(--font-body);
+		color: var(--color-text);
+		font-size: 14px;
+	}
 	.app {
 		@apply flex justify-start flex-col;
 		min-height: 100vh;
