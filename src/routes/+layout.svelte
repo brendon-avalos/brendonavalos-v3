8b9a27a6afddb629975a98c7b9621b61a350d181
brendon-avalos <57079598+brendon-avalos@users.noreply.github.com>
2023-01-17 17:55:04 -0800

    first push

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,38 @@
 </script>
 
 <slot />
+
+
+
+<style>
+	.app {
+		
+		@apply flex justify-start flex-col ;
+		min-height: 100vh;
+	}
+	
+
+	main {
+		@apply flex flex-nowrap content-start;
+		flex: 1;
+		padding: 1rem;
+		width: 100%;
+		box-sizing: border-box;
+	}
+
+	.column {
+		@apply flex flex-col gap-4;
+		flex-grow: 0;
+		min-width:min-content;
+		max-width: 24rem;
+		
+	}
+
+	
+
+		
+
+
+
+	
+</style>
